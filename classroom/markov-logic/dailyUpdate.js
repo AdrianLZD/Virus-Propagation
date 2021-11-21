8e4d27ede232f89dc2ea3e23755cff227ba9ccb1
Hugo Valdez <hugoisaac99@outlook.com>
2021-11-21 01:03:18 -0600

    add dailyUpdate module

---
+++ classroom/markov-logic/dailyUpdate.js
@@ -0,0 +1,109 @@
+import _ from 'lodash';
+
+const RISK_MULTIPLIERS = {
+    vaccine: {
+        'noVaccine': {
+            0: 1
+        },
+        'Pfizer': {
+            1: 0.76,
+            2: 0.17
+        },
+        'Moderna': {
+            1: 0.76,
+            2: 0.17
+        },
+        'Astra Zeneca': {
+            1: 0.76,
+            2: 0.47
+        },
+        'Johnson & Johnson': {
+            1: 0.36,
+        },
+        'Sputnik V': {
+            1: 0.76,
+            2: 0.17
+        },
+        'Cansino/Sinovac': {
+            1: 0.76,
+            2: 0.47
+        }
+    },
+    faceMask: {
+        'noMask': 1,
+        'clothThin': 1,
+        'clothThick': (2 / 3),
+        'surgical': (1 / 2),
+        'filter': (1 / 2),
+        'N95': (1 / 3)
+    },
+    ventilation: {
+        'none': 1,
+        'oneOutlet': (1 / 4)
+    }
+}
+
+const STATE_INDEX = {
+    S: 0,
+    I: 1,
+    EA: 2,
+    ES: 3,
+}
+
+/**
+ * @param {Student[][]} classroom
+ * @returns {Student[][]} Updated classroom (new states and transitionMatrices)
+ */
+export function dailyUpdate(classroom) {
+    const updatedClassroom = classroom.map((classroomRow, i) => {
+        return classroomRow.map((student, j) => {
+            if (student.currState == 'S') {
+                let updatedMatrix = calculateMatrixProb(i, j, classroom)
+                return { ...student, transitionMatrix: updatedMatrix };
+            }
+            return student;
+        });
+    });
+    return _.shuffle(updatedClassroom);
+}
+
+/**
+ * 
+ * @param {Number} i 
+ * @param {Number} j 
+ * @param {Student[][]} classroom 
+ * @returns 
+ */
+function calculateMatrixProb(i, j, classroom) {
+    const [m, n] = [classroom.length, classroom[0].length];
+    const neighborPositions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+
+    let infectedProb = neighborPositions.reduce((prob, nPos) => {
+        const [ni, nj] = [i + nPos[0], j + nPos[1]];
+        if (validIndex(ni, nj, m, n)) {
+            if (['I', 'EA'].includes(classroom[ni][nj].currState)) {
+                prob += 0.14 * RISK_MULTIPLIERS.faceMask[classroom[ni][nj].faceMask];
+            }
+        }
+        return prob;
+    }, 0);
+    infectedProb = _.round(
+        infectedProb * (
+            RISK_MULTIPLIERS.faceMask[classroom[i][j].faceMask]
+            * RISK_MULTIPLIERS.vaccine[classroom[i][j].vaccine.type][classroom[i][j].vaccine.dosis]
+            * RISK_MULTIPLIERS.ventilation['none']
+        ),
+        2
+    );
+    // if (infectedProb != 0) console.log('Likely to be infected: ', infectedProb);
+
+    let updatedTransitionMatrix = _.cloneDeep(classroom[i][j].transitionMatrix);
+    updatedTransitionMatrix[0][1] = infectedProb;
+    updatedTransitionMatrix[0][0] = _.round(1 - infectedProb, 2);
+
+    return updatedTransitionMatrix;
+}
+
+function validIndex(i, j, m, n) {
+    return i >= 0 && i < m && j >= 0 && j < n;
+}
--- classroom/student.js
+++ classroom/student.js
@@ -4,11 +4,10 @@ export function Vaccine(type = 'noVaccine', dosis = 0) {
 }
 
 export const DEFAULT_TRANSITION_MATRIX = [
-    [0.0000, 0.0000, 0.0000, 0.0000, 0.0000],
-    [0.0000, 0.8214, 0.0278, 0.1508, 0.0000],
-    [0.1000, 0.0000, 0.9000, 0.0000, 0.0000],
-    [0.0000, 0.0000, 0.0000, 0.0000, 1.0000],
-    [0.0714, 0.0000, 0.0000, 0.0000, 0.9286]]
+    [1.0000, 0.0000, 0.0000, 0.0000],
+    [0.0000, 0.8214, 0.0278, 0.1508],
+    [0.1000, 0.0000, 0.9000, 0.0000],
+    [0.0714, 0.0000, 0.0000, 0.9286]]
 
 export function Student(name = '', prevState = 'S', currState = 'S', vaccine = new Vaccine(), faceMask = 'noMask', transitionMatrix = DEFAULT_TRANSITION_MATRIX) {
     this.name = name;
